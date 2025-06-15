f3b33566c96a3ae7fe250f8f1b84bb5eb0ca9ef3
duvoodi <duvoodi@gmail.com>
2025-06-15 20:24:50 +0330

    chore: add folders and files for model

---
+++ Models/Courses/Course.cs
@@ -0,0 +1,15 @@
+﻿namespace AP_Project.Models.Courses
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Code { get; set; }
+        public int Unit { get; set; }
+        public string Description { get; set; }
+        public DateTime FinalExamDate { get; set; }
+
+        public ICollection<Section> Sections { get; set; }
+        public ICollection<Prerequisite> Prerequisites { get; set; }
+    }
+}
---
+++ Models/Courses/Takes.cs
@@ -0,0 +1,14 @@
+﻿using AP_Project.Models.Users;
+
+namespace AP_Project.Models.Courses
+{
+    public class Takes
+    {
+        public int StudentId { get; set; }
+        public int SectionId { get; set; }
+        public string Grade { get; set; }
+
+        public Student Student { get; set; }
+        public Section Section { get; set; }
+    }
+}
