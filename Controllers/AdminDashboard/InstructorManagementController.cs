5a0a453dfae6db0990dea53a1bc4080f9245bcba
duvoodi <duvoodi@gmail.com>
2025-07-09 23:39:57 +0330

    feat: add controller and AJAX action for instructor id generation

--- Controllers/AdminDashboard/InstructorManagementController.cs
+++ Controllers/AdminDashboard/InstructorManagementController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using AP_Project.Data;
 using AP_Project.Models.Users;
+using System.Globalization;
+using Microsoft.EntityFrameworkCore;
 
 namespace AP_Project.Controllers
 {
@@ -27,6 +29,39 @@ namespace AP_Project.Controllers
 
             ViewBag.Instructors = instructors;
             return View("~/Views/AdminDashboard/InstructorManagement/Index.cshtml", admin);
+        }
+
+        [HttpGet]
+        public IActionResult AddInstructorIndex()
+        {
+            var adminId = HttpContext.Session.GetInt32("AdminId");
+            if (adminId == null)
+                return RedirectToAction("Index", "Login");
+
+            var admin = _db.Set<Admin>().FirstOrDefault(a => a.AdminId == adminId.Value);
+            if (admin == null)
+                return RedirectToAction("Index", "Login");
+
+            var pc = new PersianCalendar();
+            int currentPersianYear = pc.GetYear(DateTime.Now);
+            ViewData["currentPersianYear"] = currentPersianYear;
+            return View("~/Views/AdminDashboard/InstructorManagement/AddInstructor.cshtml", admin);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GenerateInstructorCode(int year)
+        {
+            var adminId = HttpContext.Session.GetInt32("AdminId");
+            if (adminId == null)
+                return RedirectToAction("Index", "Login");
+
+            var admin = await _db.Set<Admin>().FirstOrDefaultAsync(a => a.AdminId == adminId.Value);
+            if (admin == null)
+                return RedirectToAction("Index", "Login");
+
+            var codeGenerator = new CodeGenerator(_db);
+            var code = await codeGenerator.GenerateInstructorCodeAsync(year);
+            return Json(code);
         }
     }
 }
