08388528c84a4670b20d4fb30e9e9c67dddb8d18
duvoodi <duvoodi@gmail.com>
2025-07-10 21:14:20 +0330

    fix: organize scripts and improve page load UX by adding visibility animation with delayed content display until scripts load
    - moved continuous session check script outside sections, loaded immediately after <head> for early session validation
    - confirmed redirect script was already correctly placed outside sections, verified its proper functionality, and added detailed comments to it
    - ensured page content remains hidden until scripts run completely, improving UX and preventing premature interaction
    - added visibility animation CSS to main page content to enhance UX and prevent user interaction before all scripts finish loading

--- wwwroot/js/Shared/popup.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-  // همه‌ی المنت‌هایی که باید بعد از مدت کوتاهی ظاهر بشن
-  const willShowPopups = document.querySelectorAll('.will-show');
-  willShowPopups.forEach(el => {
-    setTimeout(() => {
-    el.classList.add('visible');
-    }, 500); // تأخیر 500 میلی‌ثانیه‌ای
-  });
-});
